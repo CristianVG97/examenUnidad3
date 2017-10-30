36205fbf9256508ce9a4bc7d8e0ecc62c594d278
Cristian Velazquez <crisg.3080@gmail.com>
2017-10-30 11:13:55 -0600

    Actualizacion 1

--- examen.cpp
+++ examen.cpp
@@ -1,10 +1,15 @@
 #include<iostream>
 #include<stadlib>
+#include <string.h>
 using namespace std;
 
 void main()
 {
 //aqui se hara el examen
+string nom;
+cout<<"dame tu nombre humano: ";
+cin>>nom;
+cout<<"tu nombre humano es""<<nom;
 
 }
 
